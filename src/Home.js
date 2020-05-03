12f5a2bf463d43fe5a5092fa0b822d58a0cc5ab8
matheusgir <girardi.matheus@hotmail.com>
2020-05-03 15:12:09 -0300

    first commit

---
+++ src/Home.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+import { StatusBar, Dimensions } from 'react-native'
+
+import { LinearGradient } from 'expo-linear-gradient'
+
+import styled from 'styled-components/native'
+
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Movies from '../components/Movies'
+
+const recommended = [
+	require('../assets/movies/movie1.jpg'),
+	require('../assets/series/serie1.jpg'),
+	require('../assets/series/serie4.jpg'),
+    require('../assets/series/serie2.jpg'),
+    require('../assets/series/serie3.jpg')
+]
+
+const movies = [
+	require('../assets/movies/movie1.jpg'),
+	require('../assets/movies/movie2.jpg'),
+	require('../assets/movies/movie3.jpg'),
+    require('../assets/movies/movie4.jpg')
+    
+]
+
+const series = [
+	require('../assets/series/serie1.jpg'),
+	require('../assets/series/serie2.jpg'),
+	require('../assets/series/serie3.jpg'),
+    require('../assets/series/serie4.jpg'),
+    require('../assets/series/serie5.jpg'),
+    require('../assets/series/serie6.jpg')
+]
+
+const Container = styled.ScrollView`
+	flex: 1;
+	background-color: #000;
+`
+
+const Poster = styled.ImageBackground`
+	width: 100%;
+	height: ${(Dimensions.get('window').height * 81) / 100}px;
+`
+
+const Gradient = styled(LinearGradient)`
+	height: 100%;
+`
+
+const Home = () => {
+	return (
+		<>
+			<StatusBar
+				translucent
+				backgroundColor='transparent'
+				barStyle='light-content'
+			/>
+			<Container>
+				<Poster source={require('../assets/poster.jpg')}>
+					<Gradient
+						locations={[0, 0.2, 0.6, 0.93]}
+						colors={[
+							'rgba(0,0,0,0.5)',
+							'rgba(0,0,0,0.0)',
+							'rgba(0,0,0,0.0)',
+							'rgba(0,0,0,1)'
+						]}>
+						<Header />
+						<Hero />
+					</Gradient>
+				</Poster>
+				<Movies label='Recomendados' item={recommended} />
+                <Movies label='Filmes'  item={movies} />
+				<Movies label='Séries' item={series} />
+			</Container>
+		</>
+	)
+}
+
+export default Home
